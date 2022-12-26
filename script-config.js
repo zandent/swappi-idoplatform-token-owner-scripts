e0b0c6eb7ffc1c3b5106fa6fa1e292f6f983da5d
zandent <simon.zhao@mail.utoronto.ca>
2022-12-25 22:28:43 -0500

    update for gol

--- script-config.js
+++ script-config.js
@@ -1,13 +1,13 @@
 const specs = {
-  tokenOwnerKey: "",
-  newTokenAddr: "0xbA70Fc92E65d7bE07799c646c2BA4e9647bCba01",
+  tokenOwnerKey: "0x",
+  newTokenAddr: "0xa4B59aA3De2af57959C23E2c9c89a2fCB408Ce6A",
   newTokenIDOID: '1',
-  amt: '50000000000000000000000000',
-  ratioForLP: '10',
+  amt: '2000000000000000000000000',
+  ratioForLP: '0',
 
   //nodeUrl: 'http://evmtestnet.confluxrpc.com',
   nodeUrl: 'http://evm.confluxrpc.com',
-  idoplatformAddr: "0x3A550fBF9b7c3bA3D1FF4B3a2DdB74dF5333b9AD",
+  idoplatformAddr: "0x76d0Ea17B1A727F718832dc1Ba52574B0c8269aF",
 };
 
 function delay(time) {
